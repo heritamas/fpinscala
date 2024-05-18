c7d819609d46a72cfbdaad2052d13c3d46f9a585
Heri Tamas <heri.tamas@gmail.com>
2024-05-18 21:23:37 +0200

    Property testing

--- src/main/scala/fpinscala/exercises/testing/Gen.scala
+++ src/main/scala/fpinscala/exercises/testing/Gen.scala
@@ -5,12 +5,16 @@ import fpinscala.exercises.parallelism.*
 import fpinscala.exercises.parallelism.Par.Par
 import Gen.*
 import Prop.*
-import java.util.concurrent.{Executors,ExecutorService}
+
+import java.util.concurrent.{ExecutorService, Executors}
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
 shell, which you can fill in and modify while working through the chapter.
 */
+
+
+type Gen[+A] = State[RNG, A]
 
 trait Prop
 
@@ -18,14 +22,26 @@ trait Prop
 object Prop:
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 
+
 object Gen:
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = State.unit(a)
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start))
+
+  def boolean: Gen[Boolean] = State(RNG.nonNegativeInt).map(_ % 2 == 0)
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    State.sequence(List.fill(n)(g))
 
   extension [A](self: Gen[A])
-    def flatMap[B](f: A => Gen[B]): Gen[B] = ???
+    def flatMap[B](f: A => Gen[B]): Gen[B] = State.flatMap(self)(f)
 
+    def listOfN(size: Gen[Int]): Gen[List[A]] =
+      size.flatMap(n => Gen.listOfN(n, self))
+
+
+/*
 trait Gen[A]:
   def map[B](f: A => B): Gen[B] = ???
   def flatMap[B](f: A => Gen[B]): Gen[B] = ???
-
-trait SGen[+A]
+*/
